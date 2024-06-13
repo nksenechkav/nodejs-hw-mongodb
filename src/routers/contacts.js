4cfe8c3c637247ee77a523d25a3ed2defabfd27e
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-06-13 06:49:03 +0200

    add validation ID to GET-router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,12 +12,13 @@ import {
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper (getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', validateBody(updateContactSchema),ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
