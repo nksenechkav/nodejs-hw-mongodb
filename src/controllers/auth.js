8d5e88939d0114b31334f5723975c4a9b1dbbdbf
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-06-19 10:18:14 +0200

    add login and router-post-login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,6 @@
 // src/controllers/auth.js
 
-import { registerUser } from '../services/auth.js';
+import { loginUser, registerUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -10,4 +10,10 @@ export const registerUserController = async (req, res) => {
     message: 'Successfully registered a user!',
     data: user,
   });
+};
+
+export const loginUserController = async (req, res) => {
+  await loginUser(req.body);
+
+  // далі ми доповнемо цей контролер
 };
